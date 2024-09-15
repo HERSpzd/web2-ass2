75bd8cc24c8ea34b181e462bfe2ec90c3b4e2837
HERSpzd <49366653+HERSpzd@users.noreply.github.com>
2024-09-15 17:01:14 +0800

    Initialize project

---
+++ Server/crowdfunding_db.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'crowdfunding_db'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+connection.query('SELECT * FROM FUNDRAISER', (err,rows) => {
+  if(err) throw err;
+  
+  console.log(rows);
+  
+});
+
+connection.query('SELECT * FROM CATEGORY', (err,rows) => {
+  if(err) throw err;
+  
+  console.log(rows);
+  
+});
