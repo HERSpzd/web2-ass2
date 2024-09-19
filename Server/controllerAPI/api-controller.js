16675dbe64c6ebabaac5b3d1e3bd5ca2156a9d7c
HERSpzd <49366653+HERSpzd@users.noreply.github.com>
2024-09-19 21:07:36 +0800

    Implement Create GET method to retrieve all active fundraisers including the category from the  database

--- Server/controllerAPI/api-controller.js
+++ Server/controllerAPI/api-controller.js
@@ -14,4 +14,19 @@ router.get("/", (req, res)=>{
 	})
 })
 
+router.get("/fundraisers", (req, res)=>{
+ connection.query(
+   `SELECT f.*, c.NAME AS CATEGORY_NAME
+    FROM FUNDRAISER f
+    JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
+    WHERE f.ACTIVE = true; `, 
+   (err, records, fields)=> {
+    if (err) {
+      console.error("Error while retrieve the data");
+    } else {
+      res.send(records);
+    }
+ })
+})
+
 module.exports = router;
