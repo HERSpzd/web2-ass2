db033fd4356a46f594d479a35d17c3d3dcf969af
HERSpzd <49366653+HERSpzd@users.noreply.github.com>
2024-10-01 00:38:42 +0800

    Add some images to home page and fundraisers page

--- serverside/controllerAPI/api-controller.js
+++ serverside/controllerAPI/api-controller.js
@@ -1,9 +1,18 @@
+// Import crowdfunding_db module
 var dbcon = require("../crowdfunding_db");
+// Connect to the database
 var connection = dbcon.getconnection();
 connection.connect();
+// Import the Express framework
 var express = require('express');
+// Create a new router
 var router = express.Router();
 
+/**
+ * 
+ * Get all active fundraisers from the database
+ * 
+ */
 router.get("/fundraisers", (req, res) => {
   connection.query(
     `SELECT f.*, c.NAME AS CATEGORY_NAME
@@ -19,6 +28,11 @@ router.get("/fundraisers", (req, res) => {
     })
 })
 
+/**
+ * 
+ * Get categories from the database
+ * 
+ */
 router.get("/category", (req, res) => {
   connection.query("SELECT * from CATEGORY", (err, records, fields) => {
     if (err) {
@@ -29,9 +43,18 @@ router.get("/category", (req, res) => {
   })
 })
 
-
+/**
+ * 
+ * Get all active fundraisers including the category 
+ * based on criteria from the database
+ * 
+ * @params ORGANIZER
+ * @params CITY
+ * @params CATEGORY_NAME
+ * 
+ */
 router.get("/research", (req, res) => {
-  //搜索组织者
+  // Search organizer
   if (!req.query.CITY && !req.query.CATEGORY_NAME) {
     var query = `SELECT f.*, c.NAME AS CATEGORY_NAME
                FROM FUNDRAISER f
@@ -40,56 +63,62 @@ router.get("/research", (req, res) => {
                AND f.ORGANIZER = ?`;
     var parameters = [req.query.ORGANIZER];
 
+    // Search city
   } else if (!req.query.ORGANIZER && !req.query.CATEGORY_NAME) {
     var query = `SELECT f.*, c.NAME AS CATEGORY_NAME
                FROM FUNDRAISER f
                JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
                WHERE f.ACTIVE = true 
-               AND f.CITY = ?`;  //搜索城市
+               AND f.CITY = ?`;
     var parameters = [req.query.CITY];
 
+    // Search category
   } else if (!req.query.ORGANIZER && !req.query.CITY) {
     var query = `SELECT f.*, c.NAME AS CATEGORY_NAME
                FROM FUNDRAISER f
                JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
                WHERE f.ACTIVE = true 
-               AND c.NAME = ?`;  //搜索类别
+               AND c.NAME = ?`;
     var parameters = [req.query.CATEGORY_NAME];
 
 
-
+    // Search organizer, city
   } else if (!req.query.CATEGORY_NAME) {
     var query = `SELECT f.*, c.NAME AS CATEGORY_NAME
                FROM FUNDRAISER f
                JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
                WHERE f.ACTIVE = true 
                AND f.ORGANIZER = ? 
-               AND f.CITY = ?`;  //搜索组织者，城市
+               AND f.CITY = ?`;
     var parameters = [req.query.ORGANIZER, req.query.CITY];
+    // Search organizer, category
   } else if (!req.query.CITY) {
     var query = `SELECT f.*, c.NAME AS CATEGORY_NAME
                FROM FUNDRAISER f
                JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
                WHERE f.ACTIVE = true 
                AND f.ORGANIZER = ? 
-               AND c.NAME = ?`;  //搜索组织者，类别
+               AND c.NAME = ?`;
     var parameters = [req.query.ORGANIZER, req.query.CATEGORY_NAME];
+    // Search city, category
   } else if (!req.query.ORGANIZER) {
     var query = `SELECT f.*, c.NAME AS CATEGORY_NAME
                FROM FUNDRAISER f
                JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
                WHERE f.ACTIVE = true 
                AND f.CITY = ? 
-               AND c.NAME = ?`;  //搜索城市，类别
+               AND c.NAME = ?`;
     var parameters = [req.query.CITY, req.query.CATEGORY_NAME];
+    // Search organizer, city and category
   } else {
+
     var query = `SELECT f.*, c.NAME AS CATEGORY_NAME
                FROM FUNDRAISER f
                JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
                WHERE f.ACTIVE = true 
                AND f.ORGANIZER = ?
                AND f.CITY = ?
-               AND c.NAME = ?`;  // 搜索组织者，城市，类别
+               AND c.NAME = ?`;
     var parameters = [req.query.ORGANIZER, req.query.CITY, req.query.CATEGORY_NAME];
 
 
@@ -105,12 +134,20 @@ router.get("/research", (req, res) => {
     });
 });
 
+/**
+ * 
+ * Get the details of a fundraiser (by ID) 
+ * from the database
+ * 
+ * @param FUNDRAISER_ID
+ * 
+ */
 router.get("/fundraisers/:id", (req, res) => {
   connection.query(`SELECT f.*, c.NAME AS CATEGORY_NAME
                     FROM FUNDRAISER f
                     JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID 
                     WHERE F.ACTIVE = true 
-                    AND FUNDRAISER_ID = ?` 
+                    AND FUNDRAISER_ID = ?`
     , [req.params.id], (err, records, fields) => {
       if (err) {
         console.error("Error while retrieve the data");
